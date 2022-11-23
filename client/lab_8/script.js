ed3ca8b515421987b857a8b4257bbdb855c12e84
enzoregala <enzo.regala4@gmail.com>
2022-11-22 22:24:08 -0500

    lat lon

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -70,12 +70,13 @@ function filterList(array, filterInputValue) {
 
 function initMap() {
   console.log('initMap');
-  const map = L.map('map').setView([51.505, -0.09], 13);
+  const map = L.map('map').setView([38.9897 , -76.9378], 13);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
-}
+  return map;
+} 
 async function mainEvent() {
   /*
       ## Main Event
@@ -84,7 +85,7 @@ async function mainEvent() {
         If you separate your work, when one piece is complete, you can save it and trust it
     */
 
-  initMap();
+  const pageMap = initMap();
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
